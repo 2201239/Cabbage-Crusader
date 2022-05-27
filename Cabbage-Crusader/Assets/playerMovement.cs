dcb3632862b0152e0f6b6ad45dfdd24c670eea08
2201239 <2201239@edtools.psd401.net>
2022-05-26 20:31:00 -0700

    AnimationUpdate1.1

--- Cabbage-Crusader/Assets/playerMovement.cs
+++ Cabbage-Crusader/Assets/playerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public Vector2 speed = new Vector2(30, 30); //thhis changes speed
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
         float inputX = UnityEngine.Input.GetAxis("Horizontal"); //Axis is Imported from the unity engine. 
         float inputY = UnityEngine.Input.GetAxis("Vertical"); //Axis is Imported from the unity engine. 
+
+        animator.SetFloat("Speed", Mathf.Abs(inputX));
 
         Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
 
