ba4fbe0561d14038e275c23617b14da087abe99e
2201239 <2201239@edtools.psd401.net>
2022-05-27 09:00:30 -0700

    MovementUpdate1.2

--- Cabbage-Crusader/Assets/playerMovement.cs
+++ Cabbage-Crusader/Assets/playerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public Vector2 speed = new Vector2(30, 30); //this changes speed
+    private bool m_FacingRight = true;
 
     public Animator animator;
 
@@ -22,5 +23,30 @@ public class playerMovement : MonoBehaviour
 
         transform.Translate(movement);
 
+        // If the input is moving the player right and the player is facing left...
+        if (inputX > 0 && !m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (inputX < 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+
+        
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
